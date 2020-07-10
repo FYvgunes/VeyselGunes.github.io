868af2ceca2d4eff262a64a981c34ecb928b7fff
Veysel GUNES <veysel.gunes36@gmail.com>
2020-07-10 12:50:25 +0300

    Cd Themlate Added

---
+++ CV site/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV_site.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Experience()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Skills()
+        {
+            return View();
+        }
+        public ActionResult Interests()
+        {
+            return View();
+        }
+        public ActionResult Awards()
+        {
+            return View();
+        }
+
+
+    }
+}
